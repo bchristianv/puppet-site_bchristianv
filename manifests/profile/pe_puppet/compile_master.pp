3c83c70f18c3296f70e633101b3366cddf4e6363
BChristianV <bchristianv@icloud.com>
2018-05-22 10:35:27 -0700

    pe_puppet compile master profile.

---
+++ manifests/profile/pe_puppet/compile_master.pp
@@ -0,0 +1,36 @@
+# site_bchristianv::profile::pe_puppet::compile_master
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include site_bchristianv::profile::pe_puppet::compile_master
+class site_bchristianv::profile::pe_puppet::compile_master {
+
+  include firewalld
+
+  firewalld_port { 'Puppet Agent - TCP:8140':
+    ensure   => present,
+    zone     => 'public',
+    port     => 8140,
+    protocol => 'tcp',
+  }
+
+  firewalld_port { 'Puppet Orchestration - TCP:8142':
+    ensure   => present,
+    zone     => 'public',
+    port     => 8142,
+    protocol => 'tcp',
+  }
+
+  file { '/etc/puppetlabs/puppet/hiera.yaml':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/local_site/pemaster_hiera.yaml',
+  }
+
+}
+
