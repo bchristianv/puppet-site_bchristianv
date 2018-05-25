d21063728a9284293b1883ad08870fdeaed6add1
BChristianV <bchristianv@icloud.com>
2018-05-24 20:24:32 -0700

    Rename of roles fact key.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@
 # @example
 #   include site_bchristianv
 class site_bchristianv (
-  Optional[Array] $roles = $facts['local_site-roles']
+  Optional[Array] $roles = $facts['site_bchristianv-roles']
 ){
 
   if (($roles !~ Array[String]) or ($roles == [])) {
