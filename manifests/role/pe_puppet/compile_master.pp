5c9a55d605d9a01b0993cb159ee7db24134cbd93
BChristianV <bchristianv@icloud.com>
2018-11-17 12:05:16 -0800

    Updated pe_puppet::compile_master role to use profiles.

--- manifests/role/pe_puppet/compile_master.pp
+++ manifests/role/pe_puppet/compile_master.pp
@@ -9,31 +9,6 @@
 class site_bchristianv::role::pe_puppet::compile_master {
 
   include site_bchristianv::profile::base
-  include firewalld
-
-  firewalld_port { 'Puppet Agent - TCP:8140':
-    ensure   => present,
-    zone     => 'public',
-    port     => 8140,
-    protocol => 'tcp',
-  }
-
-  firewalld_port { 'Puppet Orchestration - TCP:8142':
-    ensure   => present,
-    zone     => 'public',
-    port     => 8142,
-    protocol => 'tcp',
-  }
-
-  puppet_authorization::rule { 'puppetlabs status simple':
-    match_request_path    => '/status/v1/simple',
-    match_request_type    => 'path',
-    match_request_method  => 'get',
-    allow_unauthenticated => true,
-    sort_order            => 900,
-    path                  => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
-    notify                => Service['pe-puppetserver'],
-  }
+  include site_bchristianv::profile::pe_puppet::compile_master
 
 }
-
