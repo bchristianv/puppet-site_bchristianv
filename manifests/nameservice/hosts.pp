c6ec1a477596d3041a4d907fd26b76dc7cdbd3c7
BChristianV <bchristianv@icloud.com>
2019-03-02 06:51:27 -0800

    Generalize domain name and isolate PE vs. POS hosts entries.

--- manifests/nameservice/hosts.pp
+++ manifests/nameservice/hosts.pp
@@ -8,46 +8,49 @@
 # @example
 #   include site_bchristianv::nameservice::hosts
 #
-class site_bchristianv::nameservice::hosts {
+class site_bchristianv::nameservice::hosts (
+  Enum['pe', 'pos'] $puppet_platform
+){
 
   Host {
     ensure => present,
     target => '/etc/hosts',
   }
 
-  host { 'pemom11.cracklecode.local':
-    host_aliases => ['pemom11', 'pemaster.cracklecode.local', 'pemaster'],
-    ip           => '172.16.80.11',
+  case $puppet_platform {
+    'pe': {
+      host { 'pemom11.localdomain.local':
+        host_aliases => ['pemom11', 'pemaster.localdomain.local', 'pemaster'],
+        ip           => '172.16.80.11',
+      }
+
+      host { 'pecmproxy21.localdomain.local':
+        host_aliases => ['pecmproxy21', 'pecm.localdomain.local', 'pecm', 'pemaster.localdomain.local', 'pemaster'],
+        ip           => '172.16.80.21',
+      }
+
+      host { 'pecm31.localdomain.local':
+        host_aliases => ['pecm31'],
+        ip           => '172.16.80.31',
+      }
+
+      host { 'pecm32.localdomain.local':
+        host_aliases => ['pecm32'],
+        ip           => '172.16.80.32',
+      }
+
+      host { 'peagent41.localdomain.local':
+        host_aliases => ['peagent41'],
+        ip           => '172.16.80.41',
+      }
+
+      host { 'peagent42.localdomain.local':
+        host_aliases => ['peagent42'],
+        ip           => '172.16.80.42',
+      }
+    }
+    'pos': {  }
+    default: {  }
   }
-
-#  host { 'pecmproxy21.cracklecode.local':
-#    host_aliases => ['pecmproxy21', 'pecm.cracklecode.local', 'pecm', 'pemaster.cracklecode.local', 'pemaster'],
-#    ip           => '172.16.80.21',
-#  }
-
-#  host { 'pecm31.cracklecode.local':
-#    host_aliases => ['pecm31'],
-#    ip           => '172.16.80.31',
-#  }
-
-#  host { 'pecm32.cracklecode.local':
-#    host_aliases => ['pecm32'],
-#    ip           => '172.16.80.32',
-#  }
-
-  host { 'peagent41.cracklecode.local':
-    host_aliases => ['peagent41'],
-    ip           => '172.16.80.41',
-  }
-
-#  host { 'peagent42.cracklecode.local':
-#    host_aliases => ['peagent42'],
-#    ip           => '172.16.80.42',
-#  }
-
-#  host { 'peagent49.cracklecode.local':
-#    host_aliases => ['peagent49'],
-#    ip           => '172.16.80.49',
-#  }
 
 }
