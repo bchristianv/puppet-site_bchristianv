673510a85f97309d118eb1d90f5897c249b5f927
BChristianV <bchristianv@icloud.com>
2019-03-02 16:01:53 -0800

    Add feature flags for included modules and update class documentation.

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -1,16 +1,50 @@
 # site_bchristianv::profile::base
 #
-# A description of what this class does
+# Apply a base configuration to all systems with ability to selectively
+# disable management of certain configuraitons using feature flags.
 #
-# @summary A short summary of the purpose of this class
+# @summary Apply a base configuration to all systems.
 #
 # @example
 #   include site_bchristianv::profile::base
-class site_bchristianv::profile::base {
+#
+# @param [Boolean] manage_nameservice_hosts
+#   Enable or disable management of /etc/hosts entries.
+#   Default value: true.
+#
+# @param [Boolean] manage_firewalld
+#   Enable or disable management of firewalld.
+#   Default value: true.
+#
+# @param [Boolean] manage_selinux
+#   Enable or disable management of selinux.
+#   Default value: true.
+#
+# @param [Boolean] manage_yum
+#   Enable or disable management of yum.
+#   Default value: true.
+#
+class site_bchristianv::profile::base (
+  Boolean $manage_nameservice_hosts = true,
+  Boolean $manage_firewalld = true,
+  Boolean $manage_selinux = true,
+  Boolean $manage_yum = true
+){
 
-  include site_bchristianv::nameservice::hosts
-  include firewalld
-  include selinux
-  include yum
+  if $manage_nameservice_hosts {
+    include site_bchristianv::nameservice::hosts
+  }
+
+  if $manage_firewalld {
+    include firewalld
+  }
+
+  if $manage_selinux {
+    include selinux
+  }
+
+  if $manage_yum {
+    include yum
+  }
 
 }
