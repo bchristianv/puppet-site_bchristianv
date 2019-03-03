a8249ca16e2bcdc0fb62c83807317643246bd1b3
BChristianV <bchristianv@icloud.com>
2019-03-02 18:03:06 -0800

    Document site_bchristianv::profile::pe_puppet::agent class parameters.

--- manifests/profile/pe_puppet/agent.pp
+++ manifests/profile/pe_puppet/agent.pp
@@ -7,6 +7,18 @@
 # @example
 #   include site_bchristianv::profile::pe_puppet::agent
 #
+# @param [String] master
+#   Hostname of the puppet master to configure in puppet.conf.
+#   Default value: pemaster.localdomain.local
+#
+# @param [Boolean] manage_version
+#   Whether to manage the version of the puppet agent.
+#   Default value: false
+#
+# @param [String] version
+#   The version of the puppet agent to configure for if $manage_version is true.
+#   Default value: compiling pe server version
+#
 class site_bchristianv::profile::pe_puppet::agent (
   String $master = 'pemaster.localdomain.local',
   Boolean $manage_version = false,
