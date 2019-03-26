bf3c674bb902e4d52d1c20686e8acad0b80b35c7
BChristianV <bchristianv@icloud.com>
2019-03-26 14:54:28 -0700

    Comment verbose console messages; Comment reformatting.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 #
-# Assign puppet role(s) using values from a site_roles fact, otherwise assign
-# a base profile. $site_roles is an array of strings.
+# Assign puppet role(s) using values from a $site_roles fact, otherwise
+# assign a base profile. $site_roles is an array of strings.
 #
 # Requires:
-# - The site_roles fact be created on managed nodes by another means.
+# - The $site_roles fact be created on managed nodes by another means.
 # - Role classes of the same name as the $site_roles value(s) are defined.
 #
 # @summary Assign puppet role (or roles) using value(s) from a site_roles fact.
@@ -22,12 +22,12 @@ class site_bchristianv (
   include site_bchristianv::profile::base
 
   if (($roles !~ Array[String]) or ($roles == [])) {
-    notify { "No Array[String] type found for class ${title} parameter \$roles - No roles auto-included.": }
+    # notify { "No Array[String] type found for class ${title} parameter \$roles - No roles auto-included.": }
   }
   else {
     $roles.each |String $role| {
       if ($role == '') {
-        notify { "Empty value found in class ${title} parameter \$roles.": }
+        # notify { "Empty value found in class ${title} parameter \$roles.": }
       }
       else {
         include $role
