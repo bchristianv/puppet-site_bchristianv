086a1a6483add4f9afd13120fc65f43e4070f2a7
BChristianV <bchristianv@icloud.com>
2019-03-28 09:33:33 -0700

    Get backend server options from hiera data lookup, via class parameters, instead of using hard coded values.

--- manifests/role/pe_puppet/reverse_proxy.pp
+++ manifests/role/pe_puppet/reverse_proxy.pp
@@ -8,7 +8,31 @@
 # @example
 #   include site_bchristianv::role::pe_puppet::reverse_proxy
 #
-class site_bchristianv::role::pe_puppet::reverse_proxy {
+# @param [Array[Hash]] pe_8140_backend_options_servers
+#   The `server` parameter(s) for the backend server pool listening on port 8140.
+#   i.e., the `server` parameter for the `options` array.
+#   Default value: nil.
+#
+# @param [Array[Hash]] pe_8142_backend_options_servers
+#   The `server` parameter(s) for the backend server pool listening on port 8142.
+#   i.e., the `server` parameter for the `options` array.
+#   Default value: nil.
+#
+class site_bchristianv::role::pe_puppet::reverse_proxy (
+  Array $pe_8140_backend_options_servers,
+  Array $pe_8142_backend_options_servers
+){
+
+  $backend_options = [
+    { 'balance'    => 'roundrobin' },
+    { 'mode'       => 'tcp' },
+    { 'option'     => [
+      'tcplog',
+      'httpchk GET /status/v1/simple HTTP/1.0',
+    ],
+    },
+    { 'http-check' => 'expect string running' }
+  ]
 
   include firewalld
   include selinux
@@ -84,20 +108,10 @@ class site_bchristianv::role::pe_puppet::reverse_proxy {
     ],
   }
 
-  # ToDo(bchristianv): parameterize the `server` parameter under the `options` parameter - hostname/ipaddress
+  $pe_8140_backend_options = $backend_options + $pe_8140_backend_options_servers
+
   haproxy::backend { 'pe8140_back':
-    options => [
-      { 'balance'    => 'roundrobin' },
-      { 'mode'       => 'tcp' },
-      { 'option'     => [
-        'tcplog',
-        'httpchk GET /status/v1/simple HTTP/1.0',
-        ],
-      },
-      { 'http-check' => 'expect string running' },
-      { 'server'     => 'pecm31.localdomain.local 172.16.80.31:8140 check check-ssl verify none' },
-      { 'server'     => 'pecm32.localdomain.local 172.16.80.32:8140 check check-ssl verify none' },
-    ],
+    options => $pe_8140_backend_options
   }
 
   haproxy::frontend { 'pe8142_front':
@@ -114,20 +128,10 @@ class site_bchristianv::role::pe_puppet::reverse_proxy {
     ],
   }
 
-  # ToDo(bchristianv): parameterize the `server` parameter under the `options` parameter - hostname/ipaddress
+  $pe_8142_backend_options = $backend_options + $pe_8142_backend_options_servers
+
   haproxy::backend { 'pe8142_back':
-    options => [
-      { 'balance'    => 'roundrobin' },
-      { 'mode'       => 'tcp' },
-      { 'option'     => [
-        'tcplog',
-        'httpchk GET /status/v1/simple HTTP/1.0',
-        ],
-      },
-      { 'http-check' => 'expect string running' },
-      { 'server'     => 'pecm31.localdomain.local 172.16.80.31:8142 check port 8140 check-ssl verify none' },
-      { 'server'     => 'pecm32.localdomain.local 172.16.80.32:8142 check port 8140 check-ssl verify none' },
-    ],
+    options => $pe_8142_backend_options
   }
 
 }
--- spec/classes/role/pe_puppet/reverse_proxy_spec.rb
+++ spec/classes/role/pe_puppet/reverse_proxy_spec.rb
@@ -5,6 +5,21 @@ describe 'site_bchristianv::role::pe_puppet::reverse_proxy' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
+      let(:params) do
+        {
+          'pe_8140_backend_options_servers' => [
+            {
+              'server' => 'host.localdomain.local 1.2.3.4:8140 check check-ssl verify none',
+            },
+          ],
+          'pe_8142_backend_options_servers' => [
+            {
+              'server' => 'host.localdomain.local 1.2.3.4:8142 check port 8140 check-ssl verify none',
+            },
+          ],
+        }
+      end
+
       it { is_expected.to compile }
     end
   end
