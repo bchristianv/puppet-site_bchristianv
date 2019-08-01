8e9690a08d6947df57a108076daa864f81c8b00f
BChristianV <bchristianv@icloud.com>
2019-08-01 13:42:04 -0700

    compile_master rspec test

---
+++ spec/classes/role/pos_puppet/compile_master_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'site_bchristianv::role::pos_puppet::compile_master' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
